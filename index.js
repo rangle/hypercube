135c5b5dad1dc75e6d99d32a2b03f1ba79a88d1b
Aaron Hamilton <aaron@correspondwith.me>
2015-05-21 19:43:16 +0000

    If manifest.json doesn't exist in uploadPath, initialize it.

--- index.js
+++ index.js
@@ -15,14 +15,47 @@ app.listen(config.port);
 
 assureDir(config.uploadPath + '/pr');
 assureDir(config.uploadPath + '/branch');
+assureManifest();
+
 
 function assureDir(dir) {
   fs.exists(dir, function(exists) {
     if(!exists) {
       fs.mkdir(dir, function(e) {
         if(e) {
-          console.error('Could not create' + dir + ', check config.js to make sure your uploadPath property is set to a path that you own.');
+          console.error('Could not create ' + dir + ',\n check config.js to make sure your uploadPath property is set to a path that you own.');
           throw e;
+        }
+      });
+    }
+  });
+}
+
+function assureManifest() {
+  var file = config.uploadPath + '/manifest.json';
+  fs.exists(file, function(exists) {
+    if(!exists) {
+      fs.open(file, 'w', function(e, fd) {
+        if(e) {
+          console.error('Could not open ' + file + ' for writing,\n check config.js to make sure your uploadPath property is set to a path that you own.');
+          throw e;
+        } else {
+          var data = JSON.stringify({
+            by: {
+              pr: {
+              },
+              branch: {
+              },
+              timestamp: {
+              }
+            }
+          });
+          fs.write(fd, data, function(e) {
+            if(e) {
+              console.error('Could not write default structure to ' + file + ',\n check config.js to make sure your uploadPath property is set to a path that you own.');
+              throw e;
+            }
+          });
         }
       });
     }
