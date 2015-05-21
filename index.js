f0ce2cf1f6a1ee32071a62db01bcc20801e534f3
Aaron Hamilton <aaron@correspondwith.me>
2015-05-21 16:35:54 +0000

    Automatically create pr and branch directories in uploadPath.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var config = require('./config');
 
+var fs = require('fs');
+
 var express = require('express');
 var app = express();
 var formidable = require('formidable');
@@ -10,3 +12,19 @@ app.post('/upload', handlers.upload);
 
 app.set('port', process.env.PORT || config.port);
 app.listen(config.port);
+
+assureDir(config.uploadPath + '/pr');
+assureDir(config.uploadPath + '/branch');
+
+function assureDir(dir) {
+  fs.exists(dir, function(exists) {
+    if(!exists) {
+      fs.mkdir(dir, function(e) {
+        if(e) {
+          console.error('Could not create' + dir + ', check config.js to make sure your uploadPath property is set to a path that you own.');
+          throw e;
+        }
+      });
+    }
+  });
+}
